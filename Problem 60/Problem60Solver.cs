7ede287d3be95e509a34474c2221ab57c11b178c
Rob Cilissen <rob@cilissen.nl>
2021-06-14 13:59:09 +0200

    Problem 60 New style

--- Problem 60/Problem60Solver.cs
+++ Problem 60/Problem60Solver.cs
@@ -12,26 +12,40 @@ namespace Problem_60
 {
     class Problem60Solver
     {
+        public Dictionary<int, HashSet<int>> RelationSets { get; set; }
+
         /// <summary>
         /// The number of nodes to solve the problem for
         /// </summary>
         public int NumberOfNodes { get; set; }
-        /// <summary>
-        /// The  maximum prime number to use (for performance, increase if nu result found)
-        /// </summary>
-        public int MaxPrime { get; set; }
 
         /// <summary>
         /// The number of relations required between the numbers to find the solution
         /// </summary>
-        private int NumberOfRelations {
+        private int NumberOfRelations
+        {
             get { return NumberOfNodes - 1; }
         }
+
+        /// <summary>
+        /// Contains the currently found smallest sum
+        /// </summary>
+        public int MinSum { get; set; }
 
         /// <summary>
         /// Contains the list of prime numbers for analyzing
         /// </summary>
         private List<int> PrimeNumbers { get; set; }
+
+        /// <summary>
+        /// The maximum prime number to use (for performance, increase if nu result found)
+        /// </summary>
+        public int MaxPrime { get; set; }
+
+        /// <summary>
+        /// The hashset with the lowest sum
+        /// </summary>
+        public HashSet<int> CurrentResult { get; set; }
 
         /// <summary>
         /// List representing the relations between primes that meet the criteria (concat in each order leads to prime number)
@@ -70,6 +84,9 @@ namespace Problem_60
         /// <param name="_resetSums">On true, recalculate the sums files with the primes</param>
         public Problem60Solver(PrimeNumbers _primeNumbers, bool _resetRelations = false, bool _resetSums = false)
         {
+            RelationSets = new Dictionary<int, HashSet<int>>();
+            MinSum = Int32.MaxValue;
+
             PrimeNumbers = _primeNumbers.Numbers;
             NumberOfNodes = 3; //default
 
@@ -90,40 +107,132 @@ namespace Problem_60
 
         public void Solve()
         {
-            // determine prime numbers that meet the terms
-            if (File.Exists($"{Directory.GetCurrentDirectory()}\\Relations.json") && !ResetRelations)
+            if (File.Exists($"{Directory.GetCurrentDirectory()}\\RelationSets.json") && !ResetRelations)
             {
-                string json = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\Relations.json");
-                Relations = JsonSerializer.Deserialize<List<Relation>>(json);
+                string json = File.ReadAllText($"{Directory.GetCurrentDirectory()}\\RelationSets.json");
+                RelationSets = JsonSerializer.Deserialize<Dictionary<int, HashSet<int>>>(json);
             }
             else
             {
+                // Create all sets for all primes to maxprime
+                foreach (var primeNumber in PrimeNumbers)
+                {
+                    if (primeNumber < MaxPrime)
+                    {
+                        HashSet<int> set = new HashSet<int>();
+                        set.Add(primeNumber);
+                        RelationSets.Add(primeNumber, set);
+                    }
+                }
+
+                // Add relations
                 foreach (var number in PrimeNumbers)
                 {
                     AnalyzePrime(number);
                 }
-                string json = JsonSerializer.Serialize<List<Relation>>(Relations);
-                File.WriteAllText($"{Directory.GetCurrentDirectory()}\\Relations.json", json);
+                string json = JsonSerializer.Serialize<Dictionary<int, HashSet<int>>>(RelationSets);
+                File.WriteAllText($"{Directory.GetCurrentDirectory()}\\RelationSets.json", json);
             }
 
-            // Analyze the numbers and create the nodes that are relevant
-            CreateNodes();
+            // Analyze data
 
-            // Generate combinations
-            CreateCombinations();
-
-            // Check for relations that  meet the terms
-            CheckRelations();
-
-            // Check result
-            foreach (var set in Sets.OrderBy(s => s.S))
+            // iterate all sets that have at least "NumberOfRelations" items
+            foreach (var theSet in RelationSets.Where(s => s.Value.Count >= NumberOfRelations))
             {
-                Console.WriteLine($"Found: {set.K}");
+                var path = new HashSet<int>();
+                path.Add(theSet.Key); //add first key
+
+                foreach (var currentKey in theSet.Value)
+                {
+                    // skip the first one
+                    if (theSet.Key != currentKey)
+                    {
+                        IntersectRecursively(theSet.Value, currentKey, 1, path);
+                    }
+                }
             }
+            
+            // print result
+            Console.WriteLine($"Result with lowest sum: {string.Join(';', CurrentResult)} with sum {MinSum}" );
+
             Console.WriteLine("End of script");
             Console.ReadKey();
         }
 
+
+        private void IntersectRecursively(IEnumerable<int> _sourceSet, int _targetPrime, int _level, HashSet<int> _path)
+        {
+            // set the level
+            int currentLevel = _level + 1;
+            int levelsToGo = NumberOfRelations - currentLevel;
+
+            // create the current path
+            HashSet<int> path = new();
+            path = _path.ToHashSet();
+            path.Add(_targetPrime);
+
+            // get intersection:
+            // intersection contains the relations thet needs to be examined that are left, except the already added noed to path
+            var intersection = _sourceSet.Intersect(RelationSets[_targetPrime]);
+
+            // process the last item
+            if (levelsToGo == 0)
+            {
+                // skip the current targetprime
+                if (intersection.Count() > 1) 
+                {
+                    // last iteration so the intersection sets contains the last nodes
+                    foreach (var node in intersection)
+                    {
+                        if (node != _targetPrime)
+                        {
+                            // there is a result!
+                            Console.WriteLine($"Result found {string.Join(";", path)};{node}");
+
+                            var sum = path.Sum() + node;
+                            if (sum < MinSum)
+                            {
+                                MinSum = sum;
+                                CurrentResult = path.ToHashSet();
+                                CurrentResult.Add(node);
+                            }
+                        }
+                    }
+                }
+                return;
+            }
+
+            // if the number of levels to go > the number of relations left in the intersection that stop iterating. -1 for the intersection contains the current node
+            if (intersection.Count() -1 < levelsToGo)
+            {
+                // Less items in interesection than needed items for solution
+                return; //todo: wat return?
+            }
+
+            // proceed
+            foreach (var primeNumber in intersection)
+            {
+                if (primeNumber > _targetPrime)
+                {
+                    // check if totalsum will exceed minsum
+                    if (path.Sum() + ((NumberOfNodes - currentLevel) * primeNumber) <= MinSum)
+                    {
+                        IntersectRecursively(intersection, primeNumber, currentLevel, path);
+                    }
+                }
+            }
+
+            //    if (currentLevel == NumberOfNodes && intersection.Count() >= levelsToGo)
+            //    {
+            //        // resultaat?
+            //        var sum = intersection.Sum();
+            //    }
+
+
+
+
+
+        }
 
         /// <summary>
         /// Create all combinations of nodes and split in files of 1000000 relations
@@ -307,15 +416,9 @@ namespace Problem_60
                     if (checkPrime.Equals(_prime) && PrimeNumbers.Exists(p => p.Equals(newPrime)))
                     {
                         // add to relations if not exists
-                        if (!(Relations.Exists(r => r.From.Equals(primeLeft) && r.To.Equals(primeRight))))
+                        if (primeRight < MaxPrime && primeLeft < MaxPrime)
                         {
-                            Relations.Add(new Relation() { From = primeLeft, To = primeRight });
-                            retVal = true;
-                        }
-                        if (!(Relations.Exists(r => r.From.Equals(primeRight) && r.To.Equals(primeLeft))))
-                        {
-                            Relations.Add(new Relation() { From = primeRight, To = primeLeft });
-                            retVal = true;
+                            RelationSets[Math.Min(primeLeft, primeRight)].Add(Math.Max(primeLeft, primeRight));
                         }
                     }
                 }
