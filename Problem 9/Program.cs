b7ea38e96657272727c739b634e03a70820e5054
Rob Cilissen <rob@cilissen.nl>
2021-06-25 09:33:41 +0200

    Problem 9 Solved

---
+++ Problem 9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Problem_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem 9");
+            int a = 1;
+            int b = 2;
+            int c = 1000 - b - a;
+            bool found = false;
+            
+            while (c > 0 && !found)
+            {
+                while (b < c && !found)
+                {
+                    found = found || Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2);
+                    b = !found ? b+1 : b;
+                    c = 1000 - b - a;
+                }
+                a = !found ? a + 1 : a;
+                b = !found ? a + 1 : b;
+                c = 1000 - b - a;
+            }
+            Console.WriteLine($"Result found = {found}; Result = {a}*{b}*{c}={a*b*c}");
+        }
+    }
+}
