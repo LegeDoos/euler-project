7b117afdf0a8f572a657a4fd9293c7afd28a5d04
Rob Cilissen <rob@cilissen.nl>
2021-06-25 16:27:36 +0200

    Problem 11 Solved

---
+++ Problem 11/MaxResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_11
+{
+    class MaxResult
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Int64 Product { get; set; }
+        public string Direction { get; set; }
+    }
+}
