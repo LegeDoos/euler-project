abf88f7bdc57791ca82ced2b57cc6dd43597f6e2
Rob Cilissen <rob@cilissen.nl>
2021-07-01 10:50:14 +0200

    Problem 15 Bad performance

---
+++ Problem 15/LatticePaths.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_15
+{
+    /// <summary>
+    /// Helper class to determine all lattice paths
+    /// </summary>
+    class LatticePaths
+    {
+        /// <summary>
+        /// Width of the grid
+        /// </summary>
+        public int Width { get; private set; }
+        /// <summary>
+        /// Height of the grid
+        /// </summary>
+        public int Height { get; private set; }
+        
+        public int NumberOfRoutes { get; private set; }
+        public List<string> Routes{ get; set; }
+
+        public Dictionary<string, int> StepsFromNode { get; private set; } // <"x,y", nSteps>
+                                                                           
+
+        
+        
+        public LatticePaths(int _w, int _h)
+        {
+            Width = _w;
+            Height = _h;
+            Routes = new();
+            StepsFromNode = new();
+        }
+
+
+        public void CreateRoutes(int _x, int _y, string _current = "")
+        {
+            if (_x == Width && _y == Height)
+            {
+                // done
+                Routes.Add(_current);
+                NumberOfRoutes++;
+            }
+            else
+            {
+                // next x
+                if (_x < Width)
+                {
+                    CreateRoutes(_x+1, _y, $"{_current}L"); // add one to the left
+                }
+                // next y
+                if (_y < Height)
+                {
+                    CreateRoutes(_x, _y+1, $"{_current}D"); // add one down
+                }
+            }
+
+            if (_current is null)
+            {
+                throw new ArgumentNullException(nameof(_current));
+            }
+        }
+    }
+}
---
+++ Problem 15/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem 15");
+            int start = Environment.TickCount;
+
+            LatticePaths paths = new(10, 10);
+            paths.CreateRoutes(0,0);
+
+            foreach (var route in paths.Routes)
+            {
+                Console.WriteLine(route);
+            }
+
+            Console.WriteLine($"Result: number of routes: {paths.NumberOfRoutes}");
+            Console.WriteLine($"({Environment.TickCount - start} ms)");
+        }
+
+
+    
+
+    }
+}
