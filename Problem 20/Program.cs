4f13e18adc01282a9046dfcf0ec9634cafa19d6f
Rob Cilissen <rob@cilissen.nl>
2021-07-09 13:59:12 +0200

    Problem 20 solved

---
+++ Problem 20/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem 20");
+            
+            int fact = 100;
+            BigInteger result = fact;
+            for (int i = fact-1; i > 0; i--)
+            {
+                result *= i;
+            }
+            
+            BigInteger sum = 0;
+            do
+            {
+                var digit = result % 10;
+                sum += digit;
+                result = result - digit;
+                result /= 10;
+            } while (result >= 10);
+            sum += result;
+
+            Console.WriteLine($"Result {sum}");
+        }
+    }
+}
